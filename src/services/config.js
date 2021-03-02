d530840c81bc2c8a2110ed9f5e03edc6d2a7b49c
Sayed <abusayed.wd@gmail.com>
2021-03-02 23:33:10 +0600

    API fetch issue after login solved

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,7 @@
 
 export const config = {
     BaseURL : 'https://v2.api.bandhonbd.com/',
-    BaseURL_API : 'https://v2.api.bandhonbd.com/api/'
+    BaseURL_API : 'https://v2.api.bandhonbd.com/api/',
+    ApiKey: 'b2227c16-0a50-41d5-8f60-a31dd396217',
+    TOKEN_KEY:'TOKEN_KEY'
 }
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,20 +1,14 @@
+import { config } from './config';
 import { getCookie } from './cookieService';
-
 const axios = require('axios');
-export const TOKEN_KEY = "TOKEN_KEY";
-export const API_BASE_URL = 'https://v2.api.bandhonbd.com/api/';
 
-// https://v2.api.bandhonbd.com/api/UserProfile/getProfileData/b2227c16-0a50-41d5-8f60-a31dd3962170/true
-const tokenKey = getCookie('TOKEN_KEY');
-
-
-const config = {
-    headers: { 
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${tokenKey}`
+export const getUser = () => {
+    const headerConfig = {
+        headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${getCookie(config.TOKEN_KEY)}`
+        }
     }
-}
-export const getUser = async user => {
-    // return (axios.post(API_BASE_URL+'/Login', user, { headers: { 'Content-Type': 'application/js on' }}))
-    return axios.get(API_BASE_URL+'UserProfile/getProfileData/b2227c16-0a50-41d5-8f60-a31dd3962170/true', config)
+
+    return axios.get(config.BaseURL_API+'UserProfile/getProfileData/b2227c16-0a50-41d5-8f60-a31dd3962170/true', headerConfig)
 }
